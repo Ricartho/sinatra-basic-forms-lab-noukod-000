24e28d0e4ffb267a72a2225635173171e42e9e64
Ricartho Franck <ricartho.franck@esih.edu>
2019-06-03 19:48:56 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,4 +1,5 @@
 class Puppy 
+<<<<<<< HEAD
 attr_accerssor :name,:breed,:age
 
 	def initialize(name,breed,age)
@@ -7,4 +8,12 @@ attr_accerssor :name,:breed,:age
  		@age =  age
 	end 
 
+=======
+  attr_accessor :name,:breed,:age
+  def initialize(name,breed,age)
+    @name = name
+    @breed = breed
+    @age = age
+  end 
+>>>>>>> 98948bfa3be268ee4c75b4fdf730297c6ad3ab3d
 end 
